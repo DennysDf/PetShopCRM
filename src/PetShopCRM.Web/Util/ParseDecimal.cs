a5381fd4ccf64aeceafc61e4d0c549b88e2dc270
Dennys Fonseca <dennys_ps_@LIVE.COM>
2024-05-02 09:49:15 -0300

    Ajuste

--- src/PetShopCRM.Web/Util/ParseDecimal.cs
+++ src/PetShopCRM.Web/Util/ParseDecimal.cs
@@ -3,5 +3,5 @@
 public static class ParseDecimal
 {
     public static decimal StringToDecimal(this string value) => decimal.Parse(value, NumberStyles.Currency, CultureInfo.GetCultureInfo("pt-BR"));
-    public static decimal StringLiteralToDecimal(this string value) => decimal.Parse(value[..^2] + "." + value[^2..]);
+    public static decimal StringLiteralToDecimal(this string value) => decimal.Parse(value[..^2] + "." + value[^2..], NumberStyles.Currency, CultureInfo.GetCultureInfo("pt-BR"));
 }
